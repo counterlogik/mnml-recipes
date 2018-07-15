9258a7a3471c7f6ae6bab1b894191ad638af57bf
Addison Staples <jastaplesiv@gmail.com>
2018-07-15 02:54:33 -0500

    unremove localstorage changes

--- client/src/index.js
+++ client/src/index.js
@@ -5,22 +5,35 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
+import { loadState, saveState } from "./localStorage";
 import "./css/style.css";
 import registerServiceWorker from "./registerServiceWorker";
 
 // import the root reducers
 import rootReducer from "./reducers/index";
 
-const initialState = {};
+// try to load state from localStorage
+const persistedState = loadState();
 
 // Create store with reducers and initial state and relevant enhancers
 export const store = createStore(
   rootReducer,
-  initialState,
+  persistedState,
   compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
+);
+
+store.subscribe(
+  throttle(() => {
+    saveState({
+      recipes: store.getState().recipes,
+      ingredients: store.getState().ingredients,
+      steps: store.getState().steps
+    });
+  }),
+  1000
 );
 
 render(
